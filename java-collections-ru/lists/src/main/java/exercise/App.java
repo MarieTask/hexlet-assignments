27b05e666dee88879458af3bc5dbe577ba87a539
@hexlet/cli <support@hexlet.io>
2023-02-25 02:20:19 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -1,24 +1,24 @@
 package exercise;
 
+import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.List;
 
 // BEGIN
 public class App {
     public static boolean scrabble(String symbols, String word) {
-        char[] symbolsArray = symbols.toLowerCase().toCharArray();
-        char[] wordArray = word.toLowerCase().toCharArray();
-        List<String> list = new ArrayList<>(0);
-        if (symbolsArray.length < wordArray.length) {
+        char[] symbolsArray = symbols.toLowerCase().toCharArray().split("");
+        List<String> symbolsList = new ArrayList<>(Array.asList(symbolsArray));
+        char[] wordArray = word.toLowerCase().toCharArray().split("");
+        List<String> wordList = new ArrayList<>(Array.asList(wordArray));
+        if (symbolsList.size() < wordList.size()) {
             return false;
         }
-        for (var i = 0; i < symbolsArray.length; i++) {
-            list.add(String.valueOf(symbolsArray[i]));
-        }
-        for (var j = 0; j < wordArray; j++) {
-            if (!list.contains((String.valueOf(word[k])))) {
-                return false;
+        for (var i = 0; i < symbolsList.size(); i++) {
+            if (symbolsList.contains(wordList[i])) {
+                symbolsList.remove(wordList[i]));
             } else {
-                list.remove(String.valueOf(word[k]));
+                return false;
             }
         }
         return true;
