5ab4ad3373b39a69578751c400affdbfebf8fe16
@hexlet/cli <support@hexlet.io>
2023-03-11 17:10:34 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -14,8 +14,8 @@ class AppTest {
         // BEGIN
         Integer[] num = {1, 2, 3, 4, 5};
         List<Integer> numbers = new ArrayList<>(Arrays.asList(num));
-        assertThat(take(numbers, 0)).isEqualTo(new ArrayList<>(0));
-        assertThat(take(numbers, 1)).isEqualTo(new ArrayList<>(numbers.get(1)));
+        assertThat(take(numbers, 0)).isEqualTo(new ArrayList<>(numbers.size(0)));
+        assertThat(take(numbers, 1)).isEqualTo(new ArrayList<>(numbers.get(0)));
         assertThat(take(numbers, 4)).isEqualTo(new ArrayList<>(Arrays.asList(1, 2, 3, 4)));
         assertThat(take(numbers, -1)).isEqualTo(new ArrayList<>(0));
         assertThat(take(numbers, 7)).isEqualTo(new ArrayList<>(numbers));
