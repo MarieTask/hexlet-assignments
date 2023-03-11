69fe2ca948f30575708ad11868c242de3f5329d5
@hexlet/cli <support@hexlet.io>
2023-03-11 17:40:47 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -4,21 +4,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static exercise.App.take;
 
 class AppTest {
 
     @Test
     void testTake() {
         // BEGIN
-        Integer[] num = {1, 2, 3, 4, 5};
-        List<Integer> numbers = new ArrayList<>(Arrays.asList(num));
-        assertThat(take(numbers, 0)).isEqualTo(new ArrayList<>(0));
-        assertThat(take(numbers, 1)).isEqualTo(new ArrayList<>(Arrays.asList(1)));
-        assertThat(take(numbers, 4)).isEqualTo(new ArrayList<>(Arrays.asList(1, 2, 3, 4)));
-        assertThat(take(numbers, -1)).isEqualTo(new ArrayList<>(0));
-        assertThat(take(numbers, 7)).isEqualTo(new ArrayList<>(numbers));
+        List<Integer> elements = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        int count = 3;
+        List<Integer> result = new ArrayList<>();
+        result.add(1);
+        result.add(2);
+        result.add(3);
+        List<Integer> actual = App.take(elements, count);
+        Assertions.assertThat(result)
+                .isEqualTo(actual);
         // END
     }
 }
