9bb5e4e9eecec196e603bc1f939961659a927578
@hexlet/cli <support@hexlet.io>
2023-03-11 16:59:51 +0300

    submit java-collections-ru/tests

---
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -0,0 +1,24 @@
+package exercise;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+import static exercise.App.take;
+
+class AppTest {
+
+    @Test
+    void testTake() {
+        // BEGIN
+        Integer[] num = {1, 2, 3, 4, 5};
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(num));
+        assertThat(take(numbers, 0)).isEqualTo(new ArrayList<>(0));
+        assertThat(take(numbers, 1)).isEqualTo(new ArrayList<>(numbers.get(0)));
+        assertThat(take(numbers, 4)).isEqualTo(new ArrayList<>(Arrays.asList(1, 2, 3, 4)));
+        assertThat(take(numbers, -1)).isEqualTo(new ArrayList<>(0));
+        assertThat(take(numbers, 7)).isEqualTo(new ArrayList<>(numbers));
+        // END
+    }
+}
