414a90afd6f664c60ea94231d5bb7fbd63d02561
@hexlet/cli <support@hexlet.io>
2023-04-10 18:54:35 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -7,17 +7,16 @@ import java.util.List;
 // BEGIN
 public class App {
     public static boolean scrabble(String symbols, String word) {
-        List<String> symbolsList = new ArrayList<>(Arrays.asList(symbols.toLowerCase().split("")));
-        List<String> wordList = new ArrayList<>(Arrays.asList(word.toLowerCase().split("")));
+        List<String> symbolsList = new ArrayList<>(Arrays.asList(symbols.split("")));
+        List<String> wordList = new ArrayList<>(Arrays.asList(word.split("")));
         if (symbolsList.size() < wordList.size()) {
             return false;
         }
-        for (var i = 0; i < wordList.size(); i++) {
+        for (var i = 0; i < symbolsList.size(); i++) {
             if (symbolsList.contains(wordList.get(i))) {
                 symbolsList.remove(wordList.get(i));
-            } else {
-                return false;
             }
+            return false;
         }
         return true;
     }
