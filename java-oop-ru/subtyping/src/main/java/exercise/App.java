75e70a35fa14cd4867d592b117ac039bbca0445a
@hexlet/cli <support@hexlet.io>
2023-06-12 02:36:58 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -1,15 +1,11 @@
 package exercise;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-
 // BEGIN
 class App {
     public static void swapKeyValue(KeyValueStorage database) {
         database.toMap().entrySet().stream()
-                .forEach(reverse -> database.set(reverse.getValue(), reverse.getKey()));
+                .forEach(reverse -> {
+                    database.unset(reverse.getKey()); database.set(reverse.getValue(), reverse.getKey())};);
     }
 }
 // END
