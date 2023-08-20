2a0b4bbc0b5bc2f503827b5867cf0c9a2b77bbc5
@hexlet/cli <support@hexlet.io>
2023-08-20 23:43:11 +0300

    submit java-oop-ru/reflections

--- java-oop-ru/reflections/src/main/java/exercise/Validator.java
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -2,12 +2,14 @@ package exercise;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 // BEGIN
 class Validator {
 
-    private static List<String> validate(Object obj) throws IllegalAccessException {
+    public static List<String> validate(Object obj) throws IllegalAccessException {
 
         List<String> notValidFields = new ArrayList<>();
         for (Field field : obj.getClass().getDeclaredFields()) {
@@ -24,6 +26,23 @@ class Validator {
             }
         }
         return notValidFields;
+    }
+    public static Map<String, List<String>> advancedValidate(Object obj) throws IllegalAccessException {
+        Map<String, List<String>> result = new HashMap<>();
+        for (Field field : obj.getClass().getDeclaredFields()) {
+            NotNull notNull = field.getAnnotation(NotNull.class);
+            MinLength minLength = field.getAnnotation(MinLength.class);
+            field.setAccessible(true);
+            if (notNull != null && field.get(obj) == null) {
+                result.put(field.getName(), List.of("can not be null"));
+            } else if (minLength != null) {
+                String value = (String) field.get(obj);
+                if (value.length() < minLength.minLength()) {
+                    result.put(field.getName(), List.of("length less than 4"));
+                }
+            }
+        }
+        return result;
     }
 }
 // END
--- java-oop-ru/reflections/src/test/java/exercise/ValidationTest.java
+++ java-oop-ru/reflections/src/test/java/exercise/ValidationTest.java
@@ -10,7 +10,7 @@ import java.util.Map;
 class ValidationTest {
 
     @Test
-    void testValidate() {
+    void testValidate() throws IllegalAccessException {
         Address address1 = new Address("Russia", "Ufa", "Lenina", "54", null);
         List<String> result1 = Validator.validate(address1);
         List<String> expected1 = List.of();
@@ -28,6 +28,29 @@ class ValidationTest {
     }
 
     // BEGIN
-    
+    @Test
+    void testAdvancedValidate() throws IllegalAccessException {
+        Address address4 = new Address("Russia", "Ufa", "Lenina", "54", null);
+        Map<String, List<String>> result4 = Validator.advancedValidate(address4);
+        Map<String, List<String>> expected4 = Map.of();
+        assertThat(result4).isEqualTo(expected4);
+
+        Address address5 = new Address(null, "London", "1-st street", "5", "1");
+        Map<String, List<String>> result5 = Validator.advancedValidate(address5);
+        Map<String, List<String>> expected5 = Map.of("country", List.of("can not be null"));
+        assertThat(result5).isEqualTo(expected5);
+
+        Address address6 = new Address("USA", null, null, null, "1");
+        Map<String, List<String>> result6 = Validator.advancedValidate(address6);
+        Map<String, List<String>> expected6 = Map.of("city", List.of("can not be null"), "street",
+                List.of("can not be null"), "houseNumber", List.of("can not be null"));
+        assertThat(result6).isEqualTo(expected6);
+
+        Address address7 = new Address("USA", "Texas", null, "7", "2");
+        Map<String, List<String>> result7 = Validator.advancedValidate(address7);
+        Map<String, List<String>> expected7 = Map.of("country", List.of("length less than 4"), "street",
+                List.of("can not be null"));
+        assertThat(result7).isEqualTo(expected7);
+    }
     // END
 }
